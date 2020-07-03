a5ac7b10ba636f78048fee5ed4c8334d205f647e
reps <marco@reps.cc>
2020-07-03 13:18:24 +0200

    wef

--- multimeter.py
+++ multimeter.py
@@ -10,5 +10,5 @@ class S7081:
     
     def __init__(self, address):   
         self.gpib_address = address
-        instr =  vxi11.Instrument(vxi_ip, "gpib0,"+self.address)
+        instr =  vxi11.Instrument(self.vxi_ip, "gpib0,"+self.address)
         instr.write("BEEp")
