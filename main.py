2ac31734bbcf46e2b943fc57337f3e06407a2ed5
reps <marco@reps.cc>
2020-07-03 23:19:18 +0200

    wr

--- main.py
+++ main.py
@@ -4,14 +4,11 @@
 import csv
 from datetime import datetime
 
-#from temp_sensor import TMP117
+from temp_sensor import TMP117
 from multimeter import *
 
-#short_sensor = TMP117(0x48)
-#long_sensor = TMP117(0x49)
-#print(short_sensor.read_temperature())
-#print(long_sensor.read_temperature())
-
+short_sensor = TMP117(0x48)
+long_sensor = TMP117(0x49)
 
 bench_2001 = K2001(16)
 bench_7081 = S7081(2)
