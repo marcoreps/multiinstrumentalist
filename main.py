9474c2da997d220361ba711893362dabfc8f9ec1
reps <marco@reps.cc>
2020-07-03 14:09:35 +0200

    öüä#

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 from temp_sensor import TMP117
 from multimeter import S7081
 
-#short_sensor = TMP117(0x48)
-#long_sensor = TMP117(0x49)
-#print(short_sensor.read_temperature())
-#print(long_sensor.read_temperature())
+short_sensor = TMP117(0x48)
+long_sensor = TMP117(0x49)
+print(short_sensor.read_temperature())
+print(long_sensor.read_temperature())
 
 
 bench_7081 = S7081(1)
