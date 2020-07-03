e65f487bec195aa0851e4f72e9f1c0f40c1e435f
reps <marco@reps.cc>
2020-07-03 13:36:07 +0200

    sdf

--- multimeter.py
+++ multimeter.py
@@ -7,9 +7,9 @@ import vxi11
 class S7081:
     
     vxi_ip = "192.168.178.88"
-    address = 1
+    address = 16
     
     def __init__(self, address):   
         self.gpib_address = address
         instr =  vxi11.Instrument(self.vxi_ip, "gpib0,"+str(self.address))
-        instr.write("BEEp")
+        print(instr.ask("*IDN?"))
