029f10cafeca27c96a20c6f13e64986bbc8bc81f
reps <marco@reps.cc>
2020-07-03 14:07:02 +0200

    rere

--- multimeter.py
+++ multimeter.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import vxi11
+import time
+
 
 
 class S7081:
@@ -11,5 +13,11 @@ class S7081:
     
     def __init__(self, address):   
         self.gpib_address = address
-        instr =  vxi11.Instrument(self.vxi_ip, "gpib0,"+str(self.address))
-        instr.write("BEEp")
+        self.instr =  vxi11.Instrument(self.vxi_ip, "gpib0,"+str(self.address))
+        self.instr.timeout = 60*1000
+        self.instr.write("INITIALISE")
+        time.sleep(3000)
+        self.instr.write("BEEp")
+        self.instr.write("OUTPUT, GP-IB, ON: FORMAT = ENGINEERING,EXPANDED")
+        self.instr.write("MODe=VDC: RANge=Auto: NInes=6")
+        print(self.instr.ask("MEASURE, SINGLE"))
