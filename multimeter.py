6f1ef14a139568dbda3ac9e5d1b9b9eac269ed9f
reps <marco@reps.cc>
2020-07-04 12:30:11 +0200

    ggf

--- multimeter.py
+++ multimeter.py
@@ -48,7 +48,7 @@ class K2001:
         self.instr.write(":SYST:LSYN:STAT ON")
         self.instr.write(":SENS:FUNC 'VOLT:DC'")
         #self.instr.write(":SENS:VOLT:DC:DIG 8; NPLC 10; AVER:COUN 5; TCON REP")
-        self.instr.write(":SENS:VOLT:DC:DIG 9; NPLC 10; TCON REP")
+        self.instr.write(":SENS:VOLT:DC:DIG 9; NPLC 20; TCON REP")
         self.instr.write(":SENS:VOLT:DC:AVER:STAT ON")
         self.instr.write(":SENS:VOLT:DC:RANG 20")
         self.instr.write(":FORM:ELEM READ")
