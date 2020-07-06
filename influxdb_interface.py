e0b45f3391580b061ecc941257780cef871ef1f7
reps <marco@reps.cc>
2020-07-06 13:31:21 +0200

    influxdb + grafana basics

---
+++ influxdb_interface.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from influxdb import InfluxDBClient
+from influxdb import SeriesHelper
+
+
+host = 'localhost'
+port = 8086
+user = 'grafana'
+password = 'grafana'
+dbname = 'home'
+
+myclient = InfluxDBClient(host, port, user, password, dbname)
+
+class MySeriesHelper(SeriesHelper):
+    """Instantiate SeriesHelper to write points to the backend."""
+
+    class Meta:
+        """Meta class stores time series helper configuration."""
+
+        # The client should be an instance of InfluxDBClient.
+        client = myclient
+
+        # The series name must be a string. Add dependent fields/tags
+        # in curly brackets.
+        series_name = 'events.stats.{instrument_name}'
+
+        # Defines all the fields in this time series.
+        fields = ['value']
+
+        # Defines all the tags for the series.
+        tags = ['instrument_name']
+
+        # Defines the number of data points to store prior to writing
+        # on the wire.
+        #bulk_size = 10
+
+        # autocommit must be set to True when using bulk_size
+        #autocommit = True
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import logging
 import threading
 import concurrent.futures
 
+from influxdb_interface import MySeriesHelper
+
 from datetime import datetime
 
 from instruments.temp_sensor import TMP117
@@ -22,14 +24,26 @@ instruments.append(S7081(ip=vxi_ip, gpib_address=2, title="Bench S7081"))
 instruments.append(R6581T(ip=vxi_ip, gpib_address=3, title="Bench R6581T"))
 instruments[-1].config_DCV_9digit()
 instruments.append(R6581T_temp(r6581t=instruments[-1], title="R6581T Int Temp Sensor"))
-        
-with open('multithread_test.csv', mode='w', newline='') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow([i.get_title() for i in instruments])
-    while True:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for instrument in instruments:
-                executor.submit(instrument.read)
-            executor.shutdown(wait=True)
-        writer.writerow([i.get_read_val() for i in instruments])
-        csv_file.flush()
+     
+def write_csv_forever():     
+    with open('multithread_test.csv', mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([i.get_title() for i in instruments])
+        while True:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for i in instruments:
+                    executor.submit(i.read)
+                executor.shutdown(wait=True)
+            writer.writerow([i.get_read_val() for i in instruments])
+            csv_file.flush()
+
+def write_influxdb_forever():
+        while True:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for i in instruments:
+                    executor.submit(i.read)
+                executor.shutdown(wait=True)
+            for i in instruments:
+                MySeriesHelper(instrument_name=i.get_title(), value=i.get_read_val())
+            MySeriesHelper.commit()
+
