3bc793dd76d51dc4b37f72553216ceb2706642ec
reps <marco@reps.cc>
2020-07-08 13:38:41 +0200

    kjj

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ vxi_ip = "192.168.178.88"
 instruments = []
 instruments.append(TMP117(address=0x48, title="Short Temp Sensor"))
 instruments.append(TMP117(address=0x49, title="Long Temp Sensor"))
-#instruments.append(S7081(ip=vxi_ip, gpib_address=2, title="Bench S7081"))
-instruments.append(R6581T(ip=vxi_ip, gpib_address=3, title="Bench R6581T"))
-instruments[-1].config_DCV_9digit()
-instruments.append(R6581T_temp(r6581t=instruments[-1], title="R6581T Int Temp Sensor"))
+instruments.append(S7081(ip=vxi_ip, gpib_address=2, title="Bench S7081"))
+#instruments.append(R6581T(ip=vxi_ip, gpib_address=3, title="Bench R6581T"))
+#instruments[-1].config_DCV_9digit()
+#instruments.append(R6581T_temp(r6581t=instruments[-1], title="R6581T Int Temp Sensor"))
 instruments.append(F5700A(ip=vxi_ip, gpib_address=4, title="Fluke 5700A"))
      
 def write_csv_forever():     
@@ -54,7 +54,7 @@ def write_influxdb_forever():
         logging.debug('MySeriesHelper.commit()')
         MySeriesHelper.commit()
 
-def inl_test():
+def R6581T_inl_test():
     start = -10
     step = 0.1
     end = 10
@@ -88,6 +88,38 @@ def inl_test():
         MySeriesHelper.commit()
         
     instruments[4].stby()
+    
+def S7081_inl_test():
+    start = -10
+    step = 0.1
+    end = 10
+    
+    instruments[3].oper()
+    instruments[3].out(str(start)+"V")
+    instruments[3].rangelck()
+    
+    for i in numpy.arange(start,end,step):
+    
+        instruments[3].out(str(i)+"V")
+        
+        
+        instruments[0].read()
+        MySeriesHelper(instrument_name=instruments[0].get_title(), value=float(instruments[0].get_read_val()))
+        
+        instruments[1].read()
+        MySeriesHelper(instrument_name=instruments[1].get_title(), value=float(instruments[1].get_read_val()))
+        
+        time.sleep(2)
+        
+        instruments[2].read()
+        R6581T_read_val = float(instruments[2].get_read_val())
+        MySeriesHelper(instrument_name=instruments[2].get_title(), value=R6581T_read_val)
+        MySeriesHelper(instrument_name=instruments[3].get_title(), value=i)
+        MySeriesHelper(instrument_name="difference", value=R6581T_read_val-i)
+        
+        MySeriesHelper.commit()
+        
+    instruments[3].stby()
         
 inl_test()
 
