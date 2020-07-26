fd158c352833d112625e5520eb42bc484941c8f1
reps <marco@reps.cc>
2020-07-26 11:45:38 +0200

    Keithley 237 fundamentals

--- instruments/source.py
+++ instruments/source.py
@@ -7,7 +7,117 @@ import logging
 import threading
 import re
 
+class K237:
 
+
+#define MODE_DC		                   writeToDevice("F0,0X");
+#define MODE_SWEEP	                   writeToDevice("F0,1X");
+#define DATA_FORMAT_OUTPUT_TICK        writeToDevice("G15,0,0X");
+#define DATA_FORMAT_OUTPUT_SWEEP       writeToDevice("G4,2,2X");
+#define TRIGGER_ACTION		           writeToDevice("H0X");
+
+#define FILTER_DISABLE		           writeToDevice("P0X");
+#define FILTER_2READINGS	           writeToDevice("P1X");
+#define FILTER_4READINGS	           writeToDevice("P2X");
+#define FILTER_8READINGS	           writeToDevice("P3X");
+#define FILTER_16READINGS	           writeToDevice("P4X");
+#define FILTER_32READINGS	           writeToDevice("P5X");
+
+
+self.read_val = 0.0
+    
+    def __init__(self, ip, gpib_address, lock, title='Keithley 237'):
+    self.title = title
+    logging.debug(self.title+' init started')
+    self.ip = ip
+    self.gpib_address = gpib_address
+    self.lock.acquire()
+    try:
+        self.instr =  vxi11.Instrument(self.ip, "gpib0,"+str(self.gpib_address))
+        self.instr.timeout = 60*1000
+        self.instr.clear()
+        self.instr.write("J0X") # Factory init
+        self.instr.write("F0,0X") # Source V DC
+        self.instr.write("H0X") # Immediate trigger
+        self.instr.write("G4,2,0X") # Output measure value with no prefix, no suffix
+        self.instr.write("S3X") # 20ms integration time
+        self.instr.write("V1X") # Enable 1000V range
+        self.instr.write("B7.1,0,0X") # Bias 7.1V autorange
+        self.instr.write("L1E-6,4X") # 1µA compliance 1µA range
+        self.instr.close()
+    except:
+        logging.error("Error in %s __init__" % self.title, exc_info=True)
+        pass
+    finally:
+        self.lock.release()
+        
+        
+    def oper(self):
+        self.connect()
+        try:
+            self.instr.write("N1X")
+            self.instr.close()
+        except:
+            logging.error("Error in %s oper" % self.title, exc_info=True)
+            pass
+        finally:
+            self.lock.release()
+            
+            
+    def stby(self):
+        self.connect()
+        try:
+            self.instr.write("N0X")
+            self.instr.close()
+        except:
+            logging.error("Error in %s stby" % self.title, exc_info=True)
+            pass
+        finally:
+            self.lock.release()
+        
+        
+    def connect(self):
+        logging.debug(self.title+' waiting for lock')
+        self.lock.acquire()
+        self.instr.open()
+        
+        
+    def get_title(self):
+        return self.title
+        
+        
+    def get_read_val(self):
+        logging.debug(self.title+' get_read_val started')
+        self.lock.acquire()
+        try:
+            self.read_val = self.instr.read()
+            self.instr.close()
+        except:
+            logging.error("Error in %s get_read_val" % self.title, exc_info=True)
+            pass
+        finally:
+            self.lock.release()
+        
+        logging.debug("%s reading %s" % (self.title, self.read_val))
+        return self.read_val
+
+        
+     def is_ready_to_read(self):
+        return True
+        
+        
+    def is_measuring(self):
+        return False       
+        
+    def measure(self):
+        pass
+        
+        
+        
+        
+        
+    
+    
 
 class F5700A:
 
--- main.py
+++ main.py
@@ -23,15 +23,17 @@ vxi_ip = "192.168.178.88"
 instruments = dict()
 instruments["temp_short"]=TMP117(address=0x48, title="Short Temp Sensor")
 instruments["temp_long"]=TMP117(address=0x49, title="Long Temp Sensor")
-instruments["S7081"]=S7081(ip=vxi_ip, gpib_address=6, lock=loctite, title="Bench S7081")
-instruments["2002"]=K2002(ip=vxi_ip, gpib_address=5, lock=loctite, title="2002")
+#instruments["S7081"]=S7081(ip=vxi_ip, gpib_address=6, lock=loctite, title="Bench S7081")
+#instruments["2002"]=K2002(ip=vxi_ip, gpib_address=5, lock=loctite, title="2002")
 #instruments["2002"].config_2ADC_9digit_filtered()
-instruments["2002"].config_20DCV_9digit_filtered()
-instruments["R6581T"]=R6581T(ip=vxi_ip, gpib_address=3, lock=loctite, title="Bench R6581T")
-instruments["R6581T"].config_10DCV_9digit_filtered()
-instruments["temp_R6581T"]=R6581T_temp(r6581t=instruments["R6581T"], title="R6581T Int Temp Sensor")
+#instruments["2002"].config_20DCV_9digit_filtered()
+#instruments["R6581T"]=R6581T(ip=vxi_ip, gpib_address=3, lock=loctite, title="Bench R6581T")
+#instruments["R6581T"].config_10DCV_9digit_filtered()
+#instruments["temp_R6581T"]=R6581T_temp(r6581t=instruments["R6581T"], title="R6581T Int Temp Sensor")
 
-instruments["F5700A"]=F5700A(ip=vxi_ip, gpib_address=1, lock=loctite, title="Fluke 5700A")
+instruments["K237"]=K237(ip=vxi_ip, gpib_address=8, lock=loctite, title="Bench K237")
+
+#instruments["F5700A"]=F5700A(ip=vxi_ip, gpib_address=1, lock=loctite, title="Fluke 5700A")
 
 
 def drift():
@@ -86,4 +88,11 @@ def linearity():
         instruments["F5700A"].out(str(u)+"V")
         
         
-linearity()
+def leakage():
+    while True:
+    time.sleep(5)
+    for i in instruments.values():
+        if i.is_ready_to_read():
+            MySeriesHelper(instrument_name=i.get_title(), value=float(i.get_read_val()))
+        if not i.is_measuring():
+            t = threading.Thread(target=i.measure())
