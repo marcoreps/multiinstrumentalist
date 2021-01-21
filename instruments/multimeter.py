3f23e1580d9f1a98ceccd9471a74cae0ac8d393e
reps <marco@reps.cc>
2021-01-21 20:36:13 +0100

    wef

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -466,6 +466,7 @@ class HPM7177(multimeter):
         i=0
         while len(self.readings)<self.nfilter:
             number = int.from_bytes(self.buffer[i:i+4], byteorder='big', signed=False)
+            print(self.buffer[i:i+4])
             self.readings.append(number)
             logging.debug(self.title+' number '+str(number))
             i=i+6
