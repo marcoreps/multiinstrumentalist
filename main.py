0ae2cc3306c36abb1f94ccf9521bc76ae9506f6c
reps <marco@reps.cc>
2021-01-22 17:50:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ instruments["temp_long"]=TMP117(address=0x49, title="Long Temp Sensor")
 
 def HPM_test():
     instruments["HPM1"]=HPM7177(dev='/dev/ttyUSB0', baud=921600, nfilter=10000, title='HPM7177 Unit 1')
-    instruments["HPM2"]=HPM7177(dev='/dev/ttyUSB2', baud=921600, nfilter=10000, title='HPM7177 Unit 2')
 
     while True:
         time.sleep(0.2)
