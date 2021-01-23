6490da83dd43ae16baeddb4428000c528b45194a
reps <marco@reps.cc>
2021-01-23 10:56:36 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -460,7 +460,7 @@ class HPM7177(multimeter):
         
     def process(self):
         while (len(self.readings)<self.nfilter):
-            print(self.buffer.find(b'\n'))
+            print(self.buffer.find(13))
 
 
 
