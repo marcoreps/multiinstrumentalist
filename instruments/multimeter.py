47a46ea41e439068850b0d5e5cad33d069db46a3
reps <marco@reps.cc>
2021-01-24 00:17:07 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -475,7 +475,7 @@ class HPM7177(multimeter):
                     while not serial_q.get()==b'\r':
                         pass
                     print("previous byte was b'\r'")
-                    fresh_bytes=bytearray([serial_q.get() for i in range(4)])
+                    print([serial_q.get() for i in range(4)])
                     #if not serial_q.get()==160:
                     #    break
                     number = int.from_bytes(fresh_bytes, byteorder='big', signed=False)
