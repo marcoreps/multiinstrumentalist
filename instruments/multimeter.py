d861d74c097eb9b1dbfb076ab4aa971cd9706a30
reps <marco@reps.cc>
2021-01-24 01:31:43 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -477,8 +477,6 @@ class HPM7177(multimeter):
                     if(j-i == 6):
                         number = int.from_bytes(chunk[i+1:j-1], byteorder='big', signed=False)
                         readings.append(number)
-                        print(number)
-                        print(len(readings))
                         i=i+6
 
                 mean=statistics.mean(readings)
