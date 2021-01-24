e603c34342c61771c93ea64600a6a81a85d5d91f
reps <marco@reps.cc>
2021-01-24 13:06:53 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -479,7 +479,7 @@ class HPM7177(multimeter):
                 chunk=serial_q.get()
                 i=chunk.find(13)
                 while (len(readings)<self.nfilter):
-                    index=chunk[i:].find([13,160])
+                    index=chunk[i:].find(bytes([13,160]))
                     if index == -1:
                         logging.debug(self.title+' getting a new chonk')
                         chunk=serial_q.get()
