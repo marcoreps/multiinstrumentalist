dd20789b4ad1dab3a98e7923006bc152051ae368
reps <marco@reps.cc>
2021-02-03 09:21:49 +0100

    Update temperature.py

--- instruments/temperature.py
+++ instruments/temperature.py
@@ -146,19 +146,20 @@ class HPM7177_temp:
         self.sn = sn
         self.sensor = W1ThermSensor(Sensor.DS18B20, sn)
         
-        
         self.ready_to_read = False
         self.measuring = False
         self.read_val = 0
         
-        self.serial_process = Process(target=self.read_temperature)
+        self.output_q = Queue(maxsize=1)
+        
+        self.serial_process = Process(target=self.read_temperature, args=(self.output_q,))
         self.serial_process.daemon = True
         self.serial_process.start()
         
-    def read_temperature(self):
+    def read_temperature(self, q):
         while True:
-            if not self.measuring:
-                self.measure()
+            if not q.full():
+                q.put(self.sensor.get_temperature())
             else:
                 time.sleep(0.2)
         
@@ -167,27 +168,7 @@ class HPM7177_temp:
         return self.title
         
     def get_read_val(self):
-        logging.debug(self.title+' get_read_val() '+str(self.ready_to_read))
-        self.measuring = False
-        #self.ready_to_read = False
-        logging.debug(self.title+' returning '+self.read_val)
-        return self.read_val
+        return self.output_q.get()
         
     def is_readable(self):
-        logging.debug(self.title+' is_readable(self) '+str(self.ready_to_read))
-        if self.ready_to_read:
-            logging.debug(self.title+' is ready to read')
-        else:
-            logging.debug(self.title+' is not ready to read')
-        return self.ready_to_read
-        
-    def is_measuring(self):
-        return self.measuring
-        
-    def measure(self):
-        logging.debug(self.title+' measuring now')
-        self.measuring = True
-        self.read_val = self.sensor.get_temperature()
-        logging.debug(self.title+' measured '+str(self.read_val))
-        self.ready_to_read = True
-        logging.debug(self.title+' '+str(self.ready_to_read))
+        self.output_q.full()
