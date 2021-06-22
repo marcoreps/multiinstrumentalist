d6496490321018adf04a9658ccb122b4293e1dce
reps <marco@reps.cc>
2021-06-22 18:25:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ onewire_lock = Lock()
 vxi_ip = "192.168.178.88"
 
 instruments = dict()
-instruments["temp_short"]=TMP117(address=0x48, title="Short Temp Sensor")
-instruments["temp_long"]=TMP117(address=0x49, title="Long Temp Sensor")
+instruments["temp_short"]=TMP117(address=0x49, title="Short Temp Sensor")
+instruments["temp_long"]=TMP117(address=0x48, title="Long Temp Sensor")
 #instruments["S7081"]=S7081(ip=vxi_ip, gpib_address=2, lock=gpiblock, title="Bench S7081")
 #instruments["2002"]=K2002(ip=vxi_ip, gpib_address=5, lock=gpiblock, title="2002")
 #instruments["2002"].config_2ADC_9digit_filtered()
