de61498ae7bde47940a61f03b4fb892299612df0
reps <marco@reps.cc>
2021-06-22 19:33:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def test_3458A():
 
     while True:
         now = datetime.datetime.now()
-        if not(now.minute % 10) and not(now.second % 31):
+        if not(now.minute % 10) and not(now.second):
             MySeriesHelper(instrument_name=HP3458A_temperature.get_title(), value=float(HP3458A_temperature.get_read_val()))
             time.sleep(1)
         
