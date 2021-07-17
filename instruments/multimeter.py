d950753ac2742c09e75b3f021b96828d66ca022e
reps <marco@reps.cc>
2021-07-17 08:53:42 +0200

    sd

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -593,10 +593,19 @@ class HP3458A(multimeter):
             self.instr.write("NRDGS 1,AUTO")
             self.instr.write("MEM OFF")
             self.instr.write("NDIG 9")
+        except:
+            logging.error("Error in %s config_10DCV_9digit" % self.title, exc_info=True)
+            pass
+        finally:
+            self.lock.release()
+            
+    def blank_display(self):
+        try:
+            self.connect()
             self.instr.write("DISP MSG,\"                 \"")
             self.instr.write("DISP ON")
         except:
-            logging.error("Error in %s config_10DCV_9digit" % self.title, exc_info=True)
+            logging.error("Error in %s blank_display" % self.title, exc_info=True)
             pass
         finally:
             self.lock.release()
@@ -615,8 +624,6 @@ class HP3458A(multimeter):
             self.instr.write("NRDGS 1,AUTO")
             self.instr.write("MEM OFF")
             self.instr.write("NDIG 9")
-            self.instr.write("DISP MSG,\"                 \"")
-            self.instr.write("DISP ON")
         except:
             logging.error("Error in %s config_10DCV_9digit" % self.title, exc_info=True)
             pass
@@ -637,8 +644,6 @@ class HP3458A(multimeter):
             self.instr.write("NRDGS 1,AUTO")
             self.instr.write("MEM OFF")
             self.instr.write("NDIG 9")
-            #self.instr.write("DISP MSG,\"                 \"")
-            #self.instr.write("DISP ON")
         except:
             logging.error("Error in %s config_10DCV_9digit" % self.title, exc_info=True)
             pass
