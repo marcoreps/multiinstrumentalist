fdb51d880176cb2fbae74ca34b878646afc1264c
reps <marco@reps.cc>
2021-08-09 19:53:53 +0200

    Update influxdb_interface.py

--- influxdb_interface.py
+++ influxdb_interface.py
@@ -9,12 +9,14 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 class Writer():
 
     def __init__(self):
-        token = " "
-        org = "reps"
-        bucket = "ppm"
-        client = InfluxDBClient(url="http://localhost:8086", token=token)
+        self.token = " "
+        self.org = "reps"
+        self.bucket = "ppm"
+        self.client = InfluxDBClient(url="http://localhost:8086", token=token)
+        self.write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    def write(tag, field, val, time=datetime.utcnow()):
+
+    def write(self, tag, field, val, time=datetime.utcnow()):
         point = Point("mem").tag("device", tag).field(field, val).time(time, WritePrecision.NS)
 
-        write_api.write(bucket, org, point)
+        self.write_api.write(bucket, org, point)
