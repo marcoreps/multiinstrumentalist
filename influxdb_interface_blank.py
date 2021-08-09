019f13c6121527c5c07e10dbf439170b4ecc3ab0
reps <marco@reps.cc>
2021-08-09 19:58:19 +0200

    asd

--- influxdb_interface_blank.py
+++ influxdb_interface_blank.py
@@ -10,10 +10,13 @@ class Writer():
 
     def __init__(self):
         token = " "
-        org = "reps"
-        bucket = "ppm"
+        self.org = "reps"
+        self.bucket = "ppm"
         client = InfluxDBClient(url="http://localhost:8086", token=token)
+        self.write_api = client.write_api(write_options=SYNCHRONOUS)
+
 
     def write(self, tag, field, val, time=datetime.utcnow()):
         point = Point("mem").tag("device", tag).field(field, val).time(time, WritePrecision.NS)
-        write_api.write(bucket, org, point)
+
+        self.write_api.write(self.bucket, self.org, point)
