2ebb72762a56fb00d4800b72793d1c2f67f49a7d
reps <marco@reps.cc>
2021-08-09 19:51:01 +0200

    Update influxdb_interface.py

--- influxdb_interface.py
+++ influxdb_interface.py
@@ -14,7 +14,7 @@ class Writer():
         bucket = "ppm"
         client = InfluxDBClient(url="http://localhost:8086", token=token)
 
-    def write(self, tag, field, val, time=datetime.utcnow()):
+    def write(tag, field, val, time=datetime.utcnow()):
         point = Point("mem").tag("device", tag).field(field, val).time(time, WritePrecision.NS)
 
         write_api.write(bucket, org, point)
