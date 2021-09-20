4b50d4f43f0ba23ce0b31289c1bb8326db0e4d19
reps <marco@reps.cc>
2021-09-20 23:42:39 +0200

    Update TiNs_linearity_script.py

--- csv/TiNs_linearity_script.py
+++ csv/TiNs_linearity_script.py
@@ -115,10 +115,8 @@ for i in range(-109,110,1):
         dmm2.close()
         array.extend([val])
         array2.extend([val2])
-        array3.extend([val3])
-        array4.extend([val4])
         if abs(set_value) >= 0.0005:
-            print("\033[33;1mA %.8f, ppm %.4f \033[35;1mB %.8f, ppm %.4f \033[37;1mC %.8f, ppm %.4f \033[32;1mD %.8f, ppm %.4f\033[39;0m" % (val, ((val / set_value - 1) * 1e6), val2,((val2 /set_value - 1) * 1e6), val3,((val3 / set_value - 1) * 1e6),val4,((val4 / set_value - 1) * 1e6) ))
+            print("\033[33;1mA %.8f, ppm %.4f \033[35;1mB %.8f, ppm %.4f \033[37;1mC %.8f, ppm %.4f \033[32;1mD %.8f, ppm %.4f\033[39;0m" % (val, ((val / set_value - 1) * 1e6), val2,((val2 /set_value - 1) * 1e6), 0,0,0,0 ))
             if (abs(val) >= 9.9e36) or (abs(val2) >= 9.9e36):
                 print ("Smoke escaped, check connections")
                 fluke.open()
@@ -130,16 +128,12 @@ for i in range(-109,110,1):
     median = np.median(array[1:])
     sdev2 = np.std(array2[1:])
     median2 = np.median(array2[1:])
-    sdev3 = np.std(array3[1:])
-    median3 = np.median(array3[1:])
-    sdev4 = np.std(array4[1:])
-    median4 = np.median(array4[1:])
     if abs(float(i) / 10) >= 0.0005:
         dev1 = ((median / set_value - 1) * 1e6)
         dev2 = ((median2 /set_value - 1) * 1e6)
         dev3 = ((median3 /set_value - 1) * 1e6)
         dev4 = ((median4 /set_value - 1) * 1e6)
-        print("\033[32;1mSRC %.4f \033[33;1mA %.9f, sdev5 = %.3f uV, med5 = %.8f\033[35;1mB %.9f, sdev5 = %.3f uV, med5 = %.8f \033[34;1m Delta %.4f ppm \033[37;1mC %.9f, sdev5 = %.3f uV, med5 = %.8f\033[32;1m D %.9f, sdev5 = %.3f uV, med5 = %.8f\033[39;0m" % (set_value, val, sdev*1e6, median, val2,sdev2*1e6, median2, (dev1 - dev2),  val3,sdev3*1e6, median3, val4,sdev4*1e6, median4 ))
+        print("\033[32;1mSRC %.4f \033[33;1mA %.9f, sdev5 = %.3f uV, med5 = %.8f\033[35;1mB %.9f, sdev5 = %.3f uV, med5 = %.8f \033[34;1m Delta %.4f ppm \033[37;1mC %.9f, sdev5 = %.3f uV, med5 = %.8f\033[32;1m D %.9f, sdev5 = %.3f uV, med5 = %.8f\033[39;0m" % (set_value, val, sdev*1e6, median, val2,sdev2*1e6, median2, (dev1 - dev2),0,0,0,0,0,0 ))
         dmm.open()
         dmm.write("DISP MSG,'%5.3f %2.1fV" % (dev1,set_value ))
         dmm.close()
@@ -155,7 +149,7 @@ for i in range(-109,110,1):
     temp2 = float(dmm2.read())
     dmm2.close()
     with open(log_name, 'a') as dile:
-        dile.write (time.strftime("%m%d%Y-%H:%M:%S;") + ("%.9f;%.9f;%.9f;%.9f;%.9f;%.6g;%.6g;%.6g;%.6g;%.1f;%.1f\r\n" % (set_value, median, median2, median3, median4, sdev, sdev2, sdev3, sdev4, temp, temp2)))
+        dile.write (time.strftime("%m%d%Y-%H:%M:%S;") + ("%.9f;%.9f;%.9f;%.9f;%.9f;%.6g;%.6g;%.6g;%.6g;%.1f;%.1f\r\n" % (set_value, median, median2, 0, 0, sdev, sdev2, 0, 0, temp, temp2)))
     dile.close()
 
 fluke.open()
