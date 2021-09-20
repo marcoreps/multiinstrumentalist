511509daff8f329357736627dc1b88dda68dbd43
reps <marco@reps.cc>
2021-09-20 19:11:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -186,6 +186,8 @@ def test_3458A():
 def INL_3458A():
     timestr = time.strftime("%Y%m%d-%H%M%S_")
     instruments["F5700A"]=F5700A(ip=vxi_ip, gpib_address=1, lock=gpiblock, title="Fluke 5700A")
+    instruments["3458A"]=HP3458A(ip=vxi_ip, gpib_address=22, lock=gpiblock, title="3458A")
+    instruments["3458A"].config_10DCV_9digit()
     instruments["3458B"]=HP3458A(ip=vxi_ip, gpib_address=23, lock=gpiblock, title="3458B")
     instruments["3458B"].config_10DCV_9digit()
     
