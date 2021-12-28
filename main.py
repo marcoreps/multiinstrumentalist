79fdf1489313fe8db79e5ae64d48fb37ad68b712
reps <marco@reps.cc>
2021-12-28 10:36:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -558,8 +558,6 @@ def noise_3458A():
     instruments["3458A"].config_trigger_auto()
     HP3458A_temperature=HP3458A_temp(HP3458A=instruments["3458A"], title="HP3458A Int Temp Sensor")
     
-    instruments["arroyo"]=Arroyo(dev='/dev/ttyUSB0', baud=38400, title='Arroyo TECSource')
-    
     instruments["3458B"]=HP3458A(ip=vxi_ip, gpib_address=23, lock=gpiblock, title="3458B")
     instruments["3458B"].config_10DCV_9digit()
     #instruments["3458B"].config_10OHMF_9digit()
