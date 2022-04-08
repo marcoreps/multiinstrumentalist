f3241bbdc3850328504dc7da638ac4c17411e24c
reps <marco@reps.cc>
2022-04-08 16:15:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -500,6 +500,7 @@ def acal_inst(sch, interval, priority, inst):
 def log_3458A_calparams():
 
     #instruments["temp_ADRmu1"]=TMP117(address=0x48, title="ADRmu1 Temp Sensor")
+    instruments["temp_ADRmu2"]=TMP117(address=0x4B, title="ADRmu2 Temp Sensor")
     
     #instruments["3458A"]=HP3458A(ip=vxi_ip, gpib_address=22, lock=gpiblock, title="3458A")
     instruments["3458A"]=HP3458A(ip=vxi_ip, gpib_address=22, lock=gpiblock, title="ADRmu2 3458A")
