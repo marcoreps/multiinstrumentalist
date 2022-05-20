7390299641e512d5d44dec00652c1839b1558010
reps <marco@reps.cc>
2022-05-20 22:16:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -167,6 +167,7 @@ def test_3458A():
     sch = sched.scheduler(time.time, time.sleep)
     
     i = 0
+    logging.info("Planning ahead: Measurements ...")
     while i < 60*60*24*2:
         sch.enter(i, 10, instruments["3458A"].trigger_once)
         sch.enter(i, 10, instruments["3458B"].trigger_once)
@@ -176,18 +177,21 @@ def test_3458A():
         i = i + 0.5
         
     i = 1
+    logging.info("Planning ahead: Temperature sensors ...")
     while i < 60*60*24*2:
         sch.enter(i, 10, read_inst_scanner, argument=(instruments["temp_short"], "Short Temp Sensor"))
         sch.enter(i, 10, read_inst_scanner, argument=(instruments["temp_long"], "Long Temp Sensor"))
         i = i+10
         
     i = 60*10
+    logging.info("Planning ahead: Internal temp sensors ...")
     while i < 60*60*24*2:
         sch.enter(i, 10, read_inst_scanner, argument=(HP3458A_temperature))
         sch.enter(i, 10, read_inst_scanner, argument=(HP3458B_temperature))
         i = i+60*10
         
     i = 0
+    logging.info("Planning ahead: ACALs ...")
     while i < 60*60*24*2:
         sch.enter(i, 10, acal_inst, argument=(sch, 60*60, 9, instruments["3458A"]))
         sch.enter(i, 10, acal_inst, argument=(sch, 60*60, 9, instruments["3458B"]))
@@ -195,16 +199,8 @@ def test_3458A():
         sch.enter(i, 10, read_cal_params, argument=(instruments["3458A"], ))
         sch.enter(i, 10, read_cal_params, argument=(instruments["3458B"], ))
         i = i+60*60
-
-
-
-
-    
-
-    i = i + NPLC * 0.05 + 0.5
-
-    i = i+1
         
+    logging.info("Starting schedule ...")
     sch.run()
     
              
