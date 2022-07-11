229c5e0526607d9113b90b7d5cdffb6b481b5ba9
reps <marco@reps.cc>
2022-07-11 22:44:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,17 +293,17 @@ def temperature_sweep():
     instruments["3458B"].config_trigger_auto()
     
     tmin = 19
-    tmax = 34
+    tmax = 35
     tstep = 1
-    wait_settle = 80
+    wait_settle = 180
 
     sch = sched.scheduler(time.time, time.sleep)
     sch.enter(1, 10, recursive_read_inst, argument=(sch, 2, 10, instruments["3458A"]))
     sch.enter(1, 10, recursive_read_inst, argument=(sch, 2, 10, instruments["3458B"]))
     sch.enter(1, 10, recursive_read_inst, argument=(sch, 2, 10, instruments["arroyo"]))
     i=0
-    #for t in numpy.arange(tmin, tmax+0.01, tstep):
-    for t in numpy.flip(numpy.arange(tmin, tmax+0.01, tstep)):
+    for t in numpy.arange(tmin, tmax+0.01, tstep):
+    #for t in numpy.flip(numpy.arange(tmin, tmax+0.01, tstep)):
         i+=1
         sch.enter(i*wait_settle, 9, instruments["arroyo"].out, argument=([t]))
     sch.run()
