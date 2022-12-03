e4ada1637e9014ea6f1309d4c9dd52940e164b45
reps <marco@reps.cc>
2022-12-03 22:43:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -248,8 +248,8 @@ def temperature_sweep():
     instruments["3458B"].config_NPLC(50)
     instruments["3458B"].config_trigger_auto()
     
-    tmin = 30
-    tmax = 60
+    tmin = 60
+    tmax = 80
     tstep = 1
     wait_settle = 20
 
