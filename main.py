2b1473ecef2165c519aafee1725b3d14de75e37b
reps <marco@reps.cc>
2022-12-10 14:50:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,91 +47,6 @@ instruments["temp_long"]=TMP117(address=0x4A, title="Long Temp Sensor")
 #instruments["3458A"]=3458A(ip=vxi_ip, gpib_address=22, lock=gpiblock, title="3458A")
 
 
-hpm1_poly = [2.71693612e-105,-6.18663036e-095,6.15667772e-085,-3.51535638e-075,1.27108774e-065,-3.03035855e-056,4.80713857e-047,-4.99468673e-038,3.24643372e-029,-1.19312049e-020,6.76492263e-009,-1.45232376e+001]
-hpm2_poly = [4.90537479e-105,-1.14952913e-094,1.18668695e-084,-7.10282444e-075,2.73015673e-065,-7.05222652e-056,1.24474509e-046,-1.49570850e-037,1.19440563e-028,-6.01075982e-020,6.77344822e-009,-1.45109108e+001]
-
-
-
-def HPM_test():
-    instruments["HPM1"]=HPM7177(seriallock, hpm1_poly, dev='/dev/ttyUSB0', baud=921600, nfilter=100000, title='HPM7177 Unit 1')
-    instruments["HPM2"]=HPM7177(seriallock, hpm2_poly, dev='/dev/ttyUSB1', baud=921600, nfilter=100000, title='HPM7177 Unit 2')
-    instruments["HPM1_PSU_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5bc07", title='HPM7177 Unit 1 PSU Temperature sensor')
-    instruments["HPM2_PSU_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5bc14", title='HPM7177 Unit 2 PSU Temperature sensor')
-    instruments["HPM1_MEZ_TEMP"]=HPM7177_temp(onewire_lock, "00000c7454a1", title='HPM7177 Unit 1 Mezzanine Temperature sensor')
-    instruments["HPM1_MAIN_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5f808", title='HPM7177 Unit 1 Main module Temperature sensor')
-    instruments["HPM2_MEZ_TEMP"]=HPM7177_temp(onewire_lock, "00000c745496", title='HPM7177 Unit 2 Mezzanine Temperature sensor')
-    instruments["HPM2_MAIN_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5f815", title='HPM7177 Unit 2 Main module Temperature sensor')
-
-    while True:
-        for i in instruments.values():
-            if i.is_readable():
-                MySeriesHelper(instrument_name=i.get_title(), value=float(i.get_read_val()))
-        time.sleep(0.1)
-
-def HPM_INL():
-
-    instruments["HPM1"]=HPM7177(seriallock, hpm1_poly, dev='/dev/ttyUSB0', baud=921600, nfilter=10000, title='HPM7177 Unit 1')
-    #instruments["HPM2"]=HPM7177(seriallock, hpm2_poly, dev='/dev/ttyUSB1', baud=921600, nfilter=10000, title='HPM7177 Unit 2')
-    #instruments["HPM1_PSU_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5bc07", title='HPM7177 Unit 1 PSU Temperature sensor')
-    #instruments["HPM2_PSU_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5bc14", title='HPM7177 Unit 2 PSU Temperature sensor')
-    #instruments["HPM1_MEZ_TEMP"]=HPM7177_temp(onewire_lock, "00000c7454a1", title='HPM7177 Unit 1 Mezzanine Temperature sensor')
-    #instruments["HPM1_MAIN_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5f808", title='HPM7177 Unit 1 Main module Temperature sensor')
-    #instruments["HPM2_MEZ_TEMP"]=HPM7177_temp(onewire_lock, "00000c745496", title='HPM7177 Unit 2 Mezzanine Temperature sensor')
-    #instruments["HPM2_MAIN_TEMP"]=HPM7177_temp(onewire_lock, "00000cc5f815", title='HPM7177 Unit 2 Main module Temperature sensor')
-    instruments["F5700A"]=F5700A(ip=vxi_ip, gpib_address=1, lock=gpiblock, title="Fluke 5700A")
-    
-    umin = -10
-    umax = 10
-    ustep = 0.1
-    wait_settle = 5
-    samples_per_step = 1
-    
-    instruments["F5700A"].out(str(umin)+"V")
-    instruments["F5700A"].oper()
-    instruments["F5700A"].rangelck()
-    
-    with open('csv/HPM1_formula2.csv', mode='w') as csv_file:
-        fieldnames = ['vref', 'hpm1_counts']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for u in numpy.arange(umin, umax+1, ustep):
-            instruments["F5700A"].out(str(u)+"V")
-            logging.debug('main setting source to '+str(u)+'V')
-            time.sleep(wait_settle)
-            instruments["HPM1"].measure()
-            #instruments["HPM2"].measure()
-            
-
-            for j in range(samples_per_step):
-
-
-                MySeriesHelper(instrument_name=instruments["temp_short"].get_title(), value=float(instruments["temp_short"].get_read_val()))
-                MySeriesHelper(instrument_name=instruments["temp_long"].get_title(), value=float(instruments["temp_long"].get_read_val()))
-                calibrator_out = u
-                
-                while not instruments["HPM1"].is_readable():
-                    time.sleep(0.1)
-                hpm1_out = float(instruments["HPM1"].get_read_val())
-                logging.debug('main hpm1 reporting '+str(hpm1_out))
-                
-                #while not instruments["HPM2"].is_readable():
-                #    time.sleep(0.1)
-                #hpm2_out = float(instruments["HPM2"].get_read_val())
-                #logging.debug('main hpm2 reporting '+str(hpm2_out))
-                
-                MySeriesHelper(instrument_name=instruments["HPM1"].get_title(), value=hpm1_out)
-                #MySeriesHelper(instrument_name=instruments["HPM2"].get_title(), value=hpm2_out)
-                MySeriesHelper(instrument_name=instruments["F5700A"].get_title(), value=calibrator_out)
-                    
-                MySeriesHelper(instrument_name="hpm1 ppm", value=(hpm1_out-calibrator_out)/0.00001)
-                #MySeriesHelper(instrument_name="hpm2 ppm", value=(hpm2_out-calibrator_out)/0.00001)    
-
-                writer.writerow({'vref': calibrator_out, 'hpm1_counts': hpm1_out})
-            
-
-        
-    MySeriesHelper.commit()
 
 def test_3458A():
 
@@ -248,8 +163,8 @@ def temperature_sweep():
     instruments["3458B"].config_NPLC(50)
     instruments["3458B"].config_trigger_auto()
     
-    tmin = 30
-    tmax = 80
+    tmin = 15
+    tmax = 35
     tstep = 1
     wait_settle = 20
 
@@ -654,12 +569,10 @@ def readstb_test():
 
 if __name__ == '__main__':
     try:
-        #HPM_INL()
-        #HPM_test()
-        #INL_34401()
-        test_3458A()
+
+        #test_3458A()
         #INL_3458A()
-        #temperature_sweep()
+        temperature_sweep()
         #scanner2()
         #auto_ACAL_3458A()
         #log_3458A_calparams()
