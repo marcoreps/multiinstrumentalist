6affca60a084d01c9c07cbac8189dc1f7e0865e5
reps <marco@reps.cc>
2022-12-15 22:04:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -648,11 +648,11 @@ def readstb_test():
 if __name__ == '__main__':
     try:
 
-        #test_3458A()
+        test_3458A()
         #INL_3458A()
         #temperature_sweep()
         #scanner2()
-        scanner_once()
+        #scanner_once()
         #auto_ACAL_3458A()
         #log_3458A_calparams()
         #noise_3458A()
