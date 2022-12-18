a3e7a384fdde24b2dd68f194a38d64cf8f7fed96
reps <marco@reps.cc>
2022-12-18 21:45:33 +0100

    Update csv-importer.py

--- csv-importer.py
+++ csv-importer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+import datetime
 import csv
 from influxdb_interface import influx_writer
 
@@ -11,6 +11,6 @@ with open("import.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
     for i, line in enumerate(reader):
-        datetime_object = datetime.strptime(line[0], '%Y-%m-%d %H:%M:%S')
+        datetime_object = datetime.datetime.strptime(line[0], '%Y-%m-%d %H:%M:%S')
         utc=datetime_object.astimezone(datetime.timezone.utc)
         write("ADRmu107", "3458A", line[1], utc)
