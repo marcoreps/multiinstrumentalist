fc137ed89993713e270cda364f035b8d50a54edb
reps <marco@reps.cc>
2022-12-18 21:23:30 +0100

    Update csv-importer.py

--- csv-importer.py
+++ csv-importer.py
@@ -8,4 +8,4 @@ from influxdb_interface import influx_writer
 with open("import.csv", "r") as f:
     reader = csv.reader(f, delimiter="\t")
     for i, line in enumerate(reader):
-        print 'line[{}] = {}'.format(i, line)
+        print line
