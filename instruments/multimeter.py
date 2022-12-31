670d60b9cd03c75c07ba3c13e5350481c196c1ea
reps <marco@reps.cc>
2022-12-31 11:44:41 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -163,6 +163,24 @@ class HP3458A(multimeter):
         return cal175
 
 
+class Timeout():
+  """Timeout class using ALARM signal"""
+  class Timeout(Exception): pass
+
+  def __init__(self, sec):
+    self.sec = sec
+
+  def __enter__(self):
+    signal.signal(signal.SIGALRM, self.raise_timeout)
+    signal.alarm(self.sec)
+
+  def __exit__(self, *args):
+    signal.alarm(0) # disable alarm
+
+  def raise_timeout(self, *args):
+    raise Timeout.Timeout()
+    
+
 class K182_xdevs(multimeter):
 
     def __init__(self, ip, gpib_address, lock, title='Keithley 182'):
