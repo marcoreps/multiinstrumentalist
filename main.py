e04a5a64c3c13aaf4a837881f286abb3eb3a9ea1
reps <marco@reps.cc>
2023-01-10 09:26:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -661,7 +661,7 @@ def hp3458A_diff():
     while True:
         if instruments["3458A"].is_readable():
                 t=[["wiring", "direct"],["guard","open"]]
-                writer.write("PPMhub", "ADRmu2 - ADRmu4", instruments["3458A"].get_title(), instruments["3458A"].get_read_val(), tags=t)
+                writer.write("PPMhub", "ADRmu2 - ADRmu1", instruments["3458A"].get_title(), instruments["3458A"].get_read_val(), tags=t)
         writer.write("lab_sensors", "Ambient Temp", instruments["long_tmp117"].get_title(), instruments["long_tmp117"].get_read_val())
         time.sleep(1)
         
@@ -675,14 +675,14 @@ if __name__ == '__main__':
         #INL_3458A()
         #temperature_sweep()
         #scanner2()
-        scanner_once()
+        #scanner_once()
         #auto_ACAL_3458A()
         #log_3458A_calparams()
         #noise_3458A()
         #pt100_scanner()
         #readstb_test()
         #k182()
-        #hp3458A_diff()
+        hp3458A_diff()
 
         
     except (KeyboardInterrupt, SystemExit) as exErr:
