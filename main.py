63db4139d4f4265b718184f3c0797ed63d6a613a
reps <marco@reps.cc>
2023-01-16 16:04:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ def temperature_sweep():
     tmin = 40
     tmax = 70
     tstep = 1
-    wait_settle = 40
+    wait_settle = 100
 
     sch = sched.scheduler(time.time, time.sleep)
     sch.enter(1, 10, recursive_read_inst, argument=(sch, 2, 10, instruments["3458A"], "Heater"))
