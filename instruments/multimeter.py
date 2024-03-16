e32d2ca4701dc072e40ce1459aa3bd1382937ea1
reps <marco@reps.cc>
2024-03-16 16:02:41 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -186,7 +186,7 @@ class HP34420A(multimeter):
         
     def config_DCV(self, RANG):
         logging.debug(self.title+" config_DCV")
-        self.instr.write(":CONFigure:VOLTage:DC, MAX, (@FRONt1)")
+        self.instr.write("CONFigure:VOLTage:DC, MAX, (@FRONt1)")
         self.instr.write("ROUTe:TERMinals FRONt1")
         #self.instr.write(":SENSe:VOLTage:DC:NPLCycles 100")
         self.instr.write("TRIGger:DELay 0")
