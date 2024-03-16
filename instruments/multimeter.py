5b1726a50365a5973d3d0c57b2c95e5f6e761852
reps <marco@reps.cc>
2024-03-16 12:47:44 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -211,7 +211,7 @@ class HP34420A(multimeter):
         
     def config_trigger_auto(self):
         self.instr.write("TRIGger:SOURce IMMediate")
-        self.instr.write("TRIGger:COUNt INFinity")
+        self.instr.write("TRIGger:COUNt 1024")
         self.instr.write("SAMPle:COUNt MIN")
         time.sleep(1)
         self.instr.write("INITiate")
