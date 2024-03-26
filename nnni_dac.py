b759538394d2a874129b6735adce8af329be9225
reps <marco@reps.cc>
2024-03-26 12:24:20 +0100

    Update nnni_dac.py

--- nnni_dac.py
+++ nnni_dac.py
@@ -2,16 +2,21 @@ import serial
 import pyvisa
 import csv
 from instruments.multimeter import *
+from instruments.sensor import *
 from datetime import datetime
 
-rm = pyvisa.ResourceManager()
+start = 0b00000000000000000000
+stop  = 0b11111100000000000000
+step  = 0b00000100000000000000 # 64 MSBs
+# step  = 0b00000111111111111111 # 64 MSBs plus R2R
 
-values = [
-    0, 53248, 104448, 159744, 212992,
-    263168, 315392, 365568, 421888, 472064,
-    524288, 574464, 626688, 671744, 737280,
-    786432, 851968, 885760, 951296, 1048575
-]
+tempStart = 20
+tempStop  = 40
+tempStep  = 5
+
+tempSoak = 6 # in seconds
+
+rm = pyvisa.ResourceManager()
 
 NPLC = 100
 soak = 10 # measurement soak time, DAC settles to 0.02% within 1us
@@ -24,27 +29,31 @@ instr.config_NDIG(9)
 instr.config_NPLC(NPLC)
 instr.config_trigger_hold()
 
-serial = serial.Serial("/dev/ttyACM0", 9600)
+dac = serial.Serial("/dev/ttyACM0", 9600)
 
-for n in range(number_of_runs):
-    print(n)
-    timestr = time.strftime("%Y%m%d-%H%M%S_")
-    with open('csv/'+timestr+'NNNIDAC_HP3458A_INL.csv', mode='w') as csv_file:
+arroyo=Arroyo(dev='/dev/ttyUSB0', baud=38400, title='Arroyo TECSource')
 
+with open('csv/'+timestr+'NNNIDAC_HP3458A_INL_temperature.csv', mode='w') as csv_file:
+    
+    for t in range(tempStart, tempStop + tempStep, tempStep):
+        arroyo.out(t)
+        time.sleep(tempSoak)
+        
         csv_file.write("# INL run")
         csv_file.write("# soak = "+str(soak))
         csv_file.write("# samples_per_meter_per_step = "+str(samples_per_meter_per_step))
         csv_file.write("# NPLC = "+str(NPLC))
+        csv_file.write("# Temp = " + str(t))
 
-        fieldnames = ['dac_counts', '3458A_volt']
+        fieldnames = ['dac_counts', '3458A_volt', 'arroyo_temperature']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-        
-        for v in values:
-            command=str(v)+'\n'
-            serial.write(command.encode())
+
+        for i in range(start, stop, step):
+            command=str(i)+'\n'
+            dac.write(command.encode())
             time.sleep(soak)
-        
+
             for n in range(samples_per_meter_per_step):
                 instr.trigger_once()
-                writer.writerow({'dac_counts': v, '3458A_volt': float(instr.get_read_val())})
+                writer.writerow({'dac_counts': i, '3458A_volt': float(instr.get_read_val()), 'arroyo_temperature': arroyo.get_read_val()})
