123ba2884a787784c1973e3e6c2617b31f42c037
reps <marco@reps.cc>
2024-03-29 14:41:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -492,10 +492,10 @@ def resistance_bridge_temperature_sweep():
     
     instruments["arroyo"]=Arroyo(dev='/dev/ttyUSB0', baud=38400, title='Arroyo TECSource')
     
-    tmin = 16
-    tmax = 30
+    tmin = 20
+    tmax = 60
     tstep = 0.5
-    wait_settle = 120
+    wait_settle = 60
 
     sch = sched.scheduler(time.time, time.sleep)
     sch.enter(20, 10, recursive_read_inst, argument=(sch, 20, 10, instruments["K34420A"], "VBridge"))
