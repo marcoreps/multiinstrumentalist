c04d98bb2135f3363772330c6953a6d904fdf47d
reps <marco@reps.cc>
2024-04-18 23:21:22 +0200

    Update nnni_dac.py

--- nnni_dac.py
+++ nnni_dac.py
@@ -24,23 +24,29 @@ NPLC = 100
 soak = 10 # measurement soak time, DAC settles to 0.02% within 1us
 samples_per_meter_per_step = 5
 
-instr=HP3458A(rm, 'GPIB0::22::INSTR', title='3458A')
-instr.config_DCV(10)
-instr.config_NDIG(9)
-instr.config_NPLC(NPLC)
-instr.config_trigger_hold()
+a=HP3458A(rm, 'GPIB0::22::INSTR', title='3458A')
+a.config_DCV(10)
+a.config_NDIG(9)
+a.config_NPLC(NPLC)
+a.config_trigger_hold()
+
+b=HP3458A(rm, 'GPIB0::23::INSTR', title='3458B')
+b.config_DCV(10)
+b.config_NDIG(9)
+b.config_NPLC(NPLC)
+b.config_trigger_hold()
 
 serial = serial.Serial("/dev/ttyACM0", 9600)
 timestr = time.strftime("%Y%m%d-%H%M%S_")
 
-with open('csv/'+timestr+'NNNIDAC_HP3458A_INL.csv', mode='w') as csv_file:
+with open('csv/'+timestr+'NNNIDAC_HP3458AB_INL.csv', mode='w') as csv_file:
 
     csv_file.write("# INL run")
     csv_file.write("# soak = "+str(soak))
     csv_file.write("# samples_per_meter_per_step = "+str(samples_per_meter_per_step))
     csv_file.write("# NPLC = "+str(NPLC))
 
-    fieldnames = ['dac_counts', '3458A_volt', 'ambient_temp']
+    fieldnames = ['dac_counts', '3458A_volt', '3458B_volt', 'ambient_temp']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     
@@ -52,9 +58,11 @@ with open('csv/'+timestr+'NNNIDAC_HP3458A_INL.csv', mode='w') as csv_file:
         time.sleep(soak)
     
         for n in range(samples_per_meter_per_step):
-            instr.trigger_once()
+            a.trigger_once()
             temperature_sensor_data = bme280.sample(bus, address, calibration_params)
-            writer.writerow({'dac_counts': u, '3458A_volt': float(instr.get_read_val()), 'ambient_temp': temperature_sensor_data.temperature})
+            time.sleep(1)
+            b.trigger_once()
+            writer.writerow({'dac_counts': u, '3458A_volt': float(a.get_read_val()), '3458B_volt': float(b.get_read_val()), 'ambient_temp': temperature_sensor_data.temperature})
             
         print( "Time left: "+str((datetime.now()-clock)*((stop-u)/step)))
         clock=datetime.now()
