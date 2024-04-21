f04d70589687e6b480d70f88bea994c315d9576f
reps <marco@reps.cc>
2024-04-21 16:34:55 +0200

    Update nnni_dac.py

--- nnni_dac.py
+++ nnni_dac.py
@@ -4,64 +4,86 @@ import csv
 from instruments.multimeter import *
 from datetime import datetime
 
-import smbus2
-import bme280
-port = 1
-address = 0x77
-bus = smbus2.SMBus(port)
-calibration_params = bme280.load_calibration_params(bus, address)
-
-
-
 rm = pyvisa.ResourceManager()
 
 start = 0b00000000000000000000
 stop  = 0b11111111111111111111
-#step  = 0b00000100000000000000 # 64 MSBs
-step  = 0b00000000010000000000 # step size 1024, use for fine sweep
+step  = 0b00000000010000000000
+
+# temporary container for data
+data = [[],[],[],[],[],[]]
+
+# generates DAC input codes and writes to column 0
+for i in range(start, stop, step):
+    data[0].append(i)
+
+# append extra zeroes to the end to pad number of rows
+data[0].append(0)
+data[0].append(0)
+
+# m and c values for endpoint-fit INL
+m = 0.0
+c = 0.0
 
 NPLC = 100
-soak = 5 # measurement soak time, DAC settles to 0.02% within 1us
+soak = 10 # measurement soak time, DAC settles to 0.02% within 1us
 samples_per_meter_per_step = 5
 
-a=HP3458A(rm, 'GPIB0::22::INSTR', title='3458A')
-a.config_DCV(10)
-a.config_NDIG(9)
-a.config_NPLC(NPLC)
-a.config_trigger_hold()
-
-b=HP3458A(rm, 'GPIB0::23::INSTR', title='3458B')
-b.config_DCV(10)
-b.config_NDIG(9)
-b.config_NPLC(NPLC)
-b.config_trigger_hold()
+instr=HP3458A(rm, 'GPIB0::22::INSTR', title='3458A')
+instr.config_DCV(10)
+instr.config_NDIG(9)
+instr.config_NPLC(NPLC)
+instr.config_trigger_hold()
 
 serial = serial.Serial("/dev/ttyACM0", 9600)
 timestr = time.strftime("%Y%m%d-%H%M%S_")
 
-with open('csv/'+timestr+'NNNIDAC_HP3458AB_INL.csv', mode='w') as csv_file:
+# outer loop writes to columns 1 through 5
+for i in range(1, 6, 1):
+    
+    # step one - calculate m and c values for each columm (i.e. one run)
+    # write 0 value to DAC
+    command = str(data[0][0]) + '\r'
+    serial.write(command.encode())
+    # read 0 value from 3458A
+    startVolt = float(instr.get_read_val())
+    # write stop value to DAC
+    command = str(data[0][1023]) + '\r'
+    serial.write(command.encode())
+    # read stop value from 3458A
+    stopVolt = float(inst.get_read_val())
+    
+    #calculate m and c values, they will be stored later
+    m = (stopVolt - startVolt)/(data[0][1023] - data[0][0])
+    c = stopVolt - (m * data[0][1023])
+
+    # inner loop for 1024 DAC values (indices 0 to 1023)
+    for j in range(1024):
+        # send DAC values from column 0
+        command = str(data[0][j]) + '\r'
+        serial.write(command.encode())
+        # write 3458A reading for respective col 0 value
+        data[i].append(float(inst.get_read_val()))
+    
+    # append m and c to column at the end of each run
+    data[i].append(m)
+    data[i].append(c)
+
+with open('csv/'+timestr+'NNNIDAC_HP3458A_INL.csv', mode='w') as csv_file:
 
     csv_file.write("# INL run")
     csv_file.write("# soak = "+str(soak))
     csv_file.write("# samples_per_meter_per_step = "+str(samples_per_meter_per_step))
     csv_file.write("# NPLC = "+str(NPLC))
 
-    fieldnames = ['dac_counts', '3458A_volt', '3458B_volt', 'ambient_temp']
+    fieldnames = ['dac_counts', 'Run 1', 'Run 2', 'Run 3', 'Run 4', 'Run 5']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     
     clock=datetime.now()
     
-    for u in range(start, stop+1, step):
-        command=str(u)+'\r'
-        serial.write(command.encode())
-        time.sleep(soak)
-    
-        for n in range(samples_per_meter_per_step):
-            a.trigger_once()
-            temperature_sensor_data = bme280.sample(bus, address, calibration_params)
-            b.trigger_once()
-            writer.writerow({'dac_counts': u, '3458A_volt': float(a.get_read_val()), '3458B_volt': float(b.get_read_val()), 'ambient_temp': temperature_sensor_data.temperature})
-            
+    # 1026 here because of added m and c values in each column
+    for i in range(1026):
+        writer.writerow({'dac_counts': data[0][i], 'Run 1': data[1][i], 'Run 2': data[2][i], 'Run 3': data[3][i], 'Run 4': data[4][i], 'Run 5': data[5][i]})
         print( "Time left: "+str((datetime.now()-clock)*((stop-u)/step)))
         clock=datetime.now()
