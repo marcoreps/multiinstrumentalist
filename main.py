dac5ce5ff918c33ae6baec1798afacd62936f73f
reps <marco@reps.cc>
2024-04-28 07:48:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -682,7 +682,7 @@ def nbs430():
             polarity_1_samples[sample]=reading
             logging.info("In 2 polarity read "+str(reading))
             
-        difference = mean(polarity_1_samples)+mean(polarity_1_samples))/2
+        difference = (mean(polarity_1_samples)+mean(polarity_1_samples))/2
         logging.info("Difference looks like %.*f", 5, difference)
         
         
