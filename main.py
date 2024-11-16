7e323e46b66f6897d8bbae14fd7e6bab5d26dc9d
reps <marco@reps.cc>
2024-11-16 19:28:55 +0100

    asdf

--- main.py
+++ main.py
@@ -471,11 +471,11 @@ def rms_34420A():
     #instruments["K34420A"].blank_display()
     instruments["K34420A"].config_trigger_auto()
     
-    readings = [0]*10
+    readings = [0]*50
     averages = []
     
     while True:
-        for i in range(10):
+        for i in range(50):
             readings[i] = float(instruments["K34420A"].get_read_val())
         averages.append(statistics.mean(readings))
         print(numpy.sqrt(numpy.mean(numpy.square(averages))))
