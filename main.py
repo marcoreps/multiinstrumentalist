72f3614ed1980178f71885e887d405f26ac8ad07
reps <marco@reps.cc>
2024-11-25 10:50:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -704,7 +704,7 @@ try:
     #test_W4950()
     #INL_3458A()
     #temperature_sweep()
-    scanner_once()
+    #scanner_once()
     #auto_ACAL_3458A()
     #noise_3458A()
     #pt100_scanner()
@@ -712,7 +712,7 @@ try:
     #scanner_34420A()
     #resistance_bridge_temperature_sweep()
     #test_rotary_scanner_episode_2()
-    #nbs430()
+    nbs430()
 
 
 except (KeyboardInterrupt, SystemExit) as exErr:
