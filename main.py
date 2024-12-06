2ab234d36b3aebecc3b28fabb38bfd7328a2715a
reps <marco@reps.cc>
2024-12-06 12:57:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ influx_org = config['INFLUX']['org']
 writer=influx_writer(influx_url, influx_token, influx_org)
 
 #logging.basicConfig(filename='log.log', filemode='w', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
-logging.basicConfig(level=logging.info, format='%(asctime)s %(levelname)-8s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
 logging.info("Starting ...")
 
 instruments = dict()
