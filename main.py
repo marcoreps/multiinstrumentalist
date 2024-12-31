0944b32f216205928cc769090440708ce5fa5a9e
reps <marco@reps.cc>
2025-01-01 00:58:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -469,7 +469,7 @@ def resistance_bridge_temperature_sweep():
     tmin = 18
     tmax = 28
     tstep = 0.2
-    wait_settle = 300
+    wait_settle = 900
 
     sch = sched.scheduler(time.time, time.sleep)
     sch.enter(20, 10, recursive_read_inst, argument=(sch, 20, 10, instruments["K34420A"], "VBridge"))
