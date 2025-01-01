a8267d2daf89f401054472f536961255c8b74a9d
reps <marco@reps.cc>
2025-01-01 17:14:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -694,7 +694,7 @@ def f8508a_logger():
 
     instruments["8508A"]=F8508A(rm, 'TCPIP::192.168.0.88::GPIB0,9', title='8508A')
     instruments["8508A"].config_DCV(100)
-    #instruments["8508A"].config_trigger_hold()
+    instruments["8508A"].config_trigger_hold()
     
     while True:
         #instruments["8508A"].trigger_once()
