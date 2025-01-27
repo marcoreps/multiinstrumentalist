b8df514365e7899ebbf83f274d1a2c5e89f81113
reps <marco@reps.cc>
2025-01-27 17:38:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -809,11 +809,11 @@ def resistance_bridge_reversal():
 
     
 try:
-    #test_3458A()
+    test_3458A()
     #test_W4950()
     #INL_3458A()
     #temperature_sweep()
-    scanner_once()
+    #scanner_once()
     #auto_ACAL_3458A()
     #noise_3458A()
     #pt100_scanner()
