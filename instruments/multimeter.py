325eff5617913e82a7dfa017904fe5312f5e6d46
reps <marco@reps.cc>
2025-01-29 23:56:02 +0100

    Update multimeter.py

--- instruments/multimeter.py
+++ instruments/multimeter.py
@@ -364,7 +364,7 @@ class D1281(multimeter):
         self.instr.write('*CLS')
         time.sleep(2)
         self.instr.write("*IDN?")
-        logging.info("*IDN? -> "+self.instr.read_bytes(23))
+        logging.info("*IDN? -> "+str(self.instr.read_bytes(23)))
         self.instr.read_bytes(27)
         logging.info("*OPT? -> "+self.instr.query("*OPT?"))
 
