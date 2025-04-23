0b7f48d9351a3cfe9de36d864aab8246fd3e3af8
reps <marco@reps.cc>
2025-04-23 17:36:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1106,7 +1106,7 @@ def smu_tec_perhaps():
 
     from simple_pid import PID
     
-    tstart = 24.0
+    tstart = 23.0
     tmin = 18.0
     tmax = 28.0
     k_per_hour = 1.0
@@ -1136,7 +1136,7 @@ def smu_tec_perhaps():
     
     
     #pid = PID(0.7, 0.01, 4.00, setpoint=tstart)
-    pid = PID(0.2, 0.0, 1.00, setpoint=tstart)
+    pid = PID(0.2, 0.001, 1.00, setpoint=tstart)
     pid.output_limits = (-1,1)
     
     triggered = 0
