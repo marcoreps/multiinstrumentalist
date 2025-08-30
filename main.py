981828799b7455995b7323d8fa15b728808168f9
reps <marco@reps.cc>
2025-08-30 17:09:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -400,7 +400,7 @@ def nbs430():
 
         #scanner_sources = [(1, "ADRmu1"), (2, "ADRmu3"), (3, "ADRmu4"), (4, "ADRmu9"), (5, "ADRmu11"),]
         #scanner_sources = [(1, "ADRmu1"), (2, "ADRmu3"), (3, "ADRmu12"), (4, "ADRmu20"), (5, "ADRmu15"),]
-        scanner_sources = [(2, "ADRmu11"), (1, "ADRmu3"), (3, "ADRmu12"), (4, "ADRmu20"), (5, "ADRmu15"),]
+        scanner_sources = [(1, "ADRmu11"), (2, "ADRmu3"), (3, "ADRmu12"), (4, "ADRmu20"), (5, "ADRmu15"),]
         #scanner_sources = [(1, "ADRmu4"), (2, "ADRmu9"), (3, "ADRmu12"), (4, "ADRmu20"), (5, "ADRmu15"),]
 
         
