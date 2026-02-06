967c1de50142284d1ea15549b31f8a3d93df49d6
reps <marco@reps.cc>
2026-02-06 22:22:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1156,7 +1156,7 @@ def hourly_acal():
     instruments["3458P"]=HP3458A(rm, 'TCPIP::192.168.0.5::gpib0,22', title='3458P')
     instruments["3458P"].blank_display()
     time.sleep(5)
-    instruments["3458H"]=HP3458A(rm, 'TCPIP::192.168.0.5::gpib0,20', title='3458H')
+    instruments["3458H"]=HP3458A(rm, 'TCPIP::192.168.0.5::gpib0,24', title='3458H')
     time.sleep(5)
     instruments["3458H"].blank_display()
     
